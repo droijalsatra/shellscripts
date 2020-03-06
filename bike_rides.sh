f3f5dcc3e8346111a6a898fc44832ccc97394c05
droijalsatra <38492091+droijalsatra@users.noreply.github.com>
2020-03-06 19:28:11 +0100

    Update bike_rides.sh

--- bike_rides.sh
+++ bike_rides.sh
@@ -5,18 +5,19 @@ YEAR=2020
 echo "Rides from month (2-digit format): " ; read MONTH
 
 # Calling cyclinganalytics.com API (webapp linked with Strava for extracting its data) and dump on a /tmp file
-THE_CALLING=`curl -s https://www.cyclinganalytics.com/user/3152812/list | grep "var rides" | grep -Po '"distance":.*?[^\\\]",' | grep "$YEAR-$MONTH" | awk '{ print $1, $2, $20 }' | sed 's/.$//' > /tmp/bici_rides`
+# Obviously I've hidden my athlete_ID (I am the worst athlete in the world)
+THE_CALLING=`curl -s https://www.cyclinganalytics.com/user/<ATHLETE_ID>/list | grep "var rides" | grep -Po '"distance":.*?[^\\\]",' | grep "$YEAR-$MONTH" | awk '{ print $1, $2, $20 }' | sed 's/.$//' > /tmp/bike_rides`
 
 # To improve output, let's add some returns of carriage
-PARSE=`/bin/cat /tmp/bici_rides | sed 's/,/\n/g'`
+PARSE=`/bin/cat /tmp/bike_rides | sed 's/,/\n/g'`
 
 clear
 echo -e "Strava Rides on 2020-$MONTH"
 echo -e "- - - - - - - - - - - -"
-/bin/cat /tmp/bici_rides
+/bin/cat /tmp/bike_rides
 
 # Summatory for monthly total distance
-SUMMUM=`awk '{s+=$2} END {print s}' /tmp/bici_rides`
+SUMMUM=`awk '{s+=$2} END {print s}' /tmp/bike_rides`
 
 echo ""
 echo -e "Total distance on $YEAR-$MONTH: $SUMMUM"

9d866f7877a8afd36f3adc756b4b468106157f31
droijalsatra <38492091+droijalsatra@users.noreply.github.com>
2020-06-06 11:18:22 +0200

    Update check_cert_expiration_date.sh

--- check_cert_expiration_date.sh
+++ check_cert_expiration_date.sh
@@ -2,6 +2,6 @@
 
 for certif in /etc/ssl/certs/*.pem; do
    printf '%s: %s\n' \
-      "$(date --date="$(openssl x509 -enddate -noout -in "$certif"|cut -d= -f 2)" --iso-8601)" \
+      "$(date --date="$(openssl x509 -enddate -noout -in "$certif"|cut -d = -f 2)" --iso-8601)" \
       "$certif"
 done | sort
